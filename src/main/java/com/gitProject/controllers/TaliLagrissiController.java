edeb59f248ab79e1b14be6ae86e1fe2a5479c389
Dudu <dudu.avital2@gmail.com>
2022-07-05 16:23:11 +0300

    update 6

--- src/main/java/com/gitProject/controllers/TaliLagrissiController.java
+++ src/main/java/com/gitProject/controllers/TaliLagrissiController.java
@@ -2,7 +2,6 @@ package com.gitProject.controllers;
 
 import com.gitProject.dto.TaliLagrissi;
 import com.gitProject.logic.TaliLagrissiLogic;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
