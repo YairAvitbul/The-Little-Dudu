9020622aab920d980c75269d08f0cfaa8bfeea79
Dudu <dudu.avital2@gmail.com>
2022-07-05 15:40:21 +0300

    update 6

--- src/main/java/com/gitProject/Main.java
+++ src/main/java/com/gitProject/Main.java
@@ -1,4 +1,4 @@
-package com.tali;
+package com.gitProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/tali/repositories/ITestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tali.repositories;
-
-import com.tali.entities.TestEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ITestRepository extends CrudRepository<TestEntity, Long> {
-}
