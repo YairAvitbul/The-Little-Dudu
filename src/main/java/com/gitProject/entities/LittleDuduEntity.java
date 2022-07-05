140e9faf4d0b6ce745390a05a07984154f08d7d2
Dudu <dudu.avital2@gmail.com>
2022-07-05 16:12:28 +0300

    update 6

--- src/main/java/com/gitProject/entities/LittleDuduEntity.java
+++ src/main/java/com/gitProject/entities/LittleDuduEntity.java
@@ -2,16 +2,14 @@ package com.gitProject.entities;
 
 import com.gitProject.dto.LittleDudu;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "littleDudu")
 public class LittleDuduEntity {
 
     @Id
+    @GeneratedValue
     private long id;
 
     @Column(name = "name")
